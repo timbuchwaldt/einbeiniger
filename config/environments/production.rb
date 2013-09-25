bf658ff65e19e42d402bd3ab844545a6ad701520
Tim Buchwaldt <tim@buchwaldt.ws>
2013-09-25 19:38:59 +0200

    Make fallbacks work for production as well

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,7 +67,7 @@ Einbeiniger::Application.configure do
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found).
-  config.i18n.fallbacks = true
+  config.i18n.fallbacks = [:en]
 
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
@@ -80,7 +80,7 @@ Einbeiniger::Application.configure do
 end
 
 ActionMailer::Base.smtp_settings = {
-  :port           => ENV['MAILGUN_SMTP_PORT'], 
+  :port           => ENV['MAILGUN_SMTP_PORT'],
   :address        => ENV['MAILGUN_SMTP_SERVER'],
   :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
   :password       => ENV['MAILGUN_SMTP_PASSWORD'],
