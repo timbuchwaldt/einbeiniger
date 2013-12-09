4cd34c307326c3504ba3525c73880a406d6b99d4
Klaus Zanders <klaus.zanders@gmail.com>
2013-12-09 22:52:18 +0100

    Foundation initializer

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,8 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require foundation
 //= require turbolinks
-//= require twitter/bootstrap
 //= require_tree .
+
+$(function(){ $(document).foundation(); });
